2c27bd7bca65226a7e0346ed70cc433eacdc5c51
Vyacheslav Spirin <v.spirin@kefirgames.com>
2020-02-23 13:02:21 +0400

    skill icons

---
+++ Assets/Storm/Scripts/SkillIconView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillIconView : MonoBehaviour
+{
+    public Image icon;
+
+    public int charId;
+
+    private PlayerController _player;
+
+    private void Awake()
+    {
+        _player = FindObjectOfType<PlayerController>();
+    }
+
+    void Update()
+    {
+        icon.enabled = _player.GetCharId() == charId;
+    }
+}
