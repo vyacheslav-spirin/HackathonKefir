2edd1e6568b3ba71ae741dca1cb403ded87d82d8
Vyacheslav Spirin <v.spirin@kefirgames.com>
2020-02-23 09:21:55 +0400

    clean up

---
+++ Assets/Editor/Cleaner.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public static class Cleaner
+    {
+        [MenuItem("Help/Clean Up")]
+        private static void CleanUp()
+        {
+            RemoveDirs(Path.Combine(Directory.GetCurrentDirectory(), "Assets"));
+            
+            AssetDatabase.Refresh();
+        }
+    
+        private static void RemoveDirs(string startLocation)
+        {
+            foreach (var directory in Directory.GetDirectories(startLocation))
+            {
+                RemoveDirs(directory);
+                if (Directory.GetFiles(directory).Length == 0 && 
+                    Directory.GetDirectories(directory).Length == 0)
+                {
+                    Directory.Delete(directory, false);
+                }
+            }
+        }
+    }
+}
