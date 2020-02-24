02cbc93b721d2f70e384577ebccfafdab98d9f28
Vyacheslav Spirin <v.spirin@kefirgames.com>
2020-02-24 15:31:36 +0400

    key system

---
+++ Assets/Storm/Scripts/KeyPlace.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class KeyPlace : MonoBehaviour
+{
+    public GameObject key;
+
+    public int keyIndex;
+    
+    private bool used;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (used) return;
+        
+        if (other == PlayerController.Instance.actorCollider)
+        {
+            used = true;
+            
+            key.SetActive(false);
+            
+            PlayerController.AddKey(keyIndex);
+        }
+    }
+}
---
+++ Assets/Storm/Scripts/LockPlace.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class LockPlace : MonoBehaviour
+{
+    public Renderer lockRenderer;
+    
+    public int keyIndex;
+
+    public Door door;
+    
+    private bool used;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (used) return;
+        
+        if (other == PlayerController.Instance.actorCollider)
+        {
+            used = true;
+            
+            lockRenderer.material.color = new Color32(31, 255, 42, 255);
+            
+            door.Open();
+        }
+    }
+}
