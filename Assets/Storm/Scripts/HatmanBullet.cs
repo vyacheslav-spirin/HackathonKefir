c2faa0865060210c935c66ce4f2a70fc3b5b7b0e
Vyacheslav Spirin <v.spirin@kefirgames.com>
2020-02-24 14:00:01 +0400

    hatmab bullet

--- Assets/Storm/Scripts/HatmanBullet.cs
+++ Assets/Storm/Scripts/HatmanBullet.cs
@@ -13,6 +13,18 @@ public class HatmanBullet : MonoBehaviour
     public void SetTargetPoint(Vector3 point)
     {
         targetPoint = point;
+        
+        var dir = new Vector2(targetPoint.x, targetPoint.y) - new Vector2(transform.position.x, transform.position.y);
+        dir.Normalize();
+
+        if (dir != Vector2.zero)
+        {
+            var rad = Mathf.Atan2(dir.y, dir.x);
+            
+            if(rad > Mathf.PI / 2) transform.localScale = new Vector3(transform.localScale.x, -transform.localScale.y, transform.localScale.z);
+            
+            transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * rad);
+        }
     }
 
     private void Awake()
