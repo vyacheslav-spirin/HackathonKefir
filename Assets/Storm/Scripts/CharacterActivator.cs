fe57daf0f67bcaf677c75de017cd9f254462552f
Vyacheslav Spirin <v.spirin@kefirgames.com>
2020-02-24 16:38:41 +0400

    chars

--- Assets/Storm/Scripts/CharacterActivator.cs
+++ Assets/Storm/Scripts/CharacterActivator.cs
@@ -4,11 +4,15 @@ public class CharacterActivator : MonoBehaviour
 {
     public int id;
 
+    public GameObject charObject;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other == PlayerController.Instance.actorCollider)
         {
             PlayerController.AddCharacter(id);
+            
+            charObject.SetActive(false);
         }
     }
 }
