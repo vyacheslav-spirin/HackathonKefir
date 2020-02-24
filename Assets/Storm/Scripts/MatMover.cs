c3ce9a8be8272e0cacacd7c42c19183d1bfa2fcf
Vyacheslav Spirin <v.spirin@kefirgames.com>
2020-02-24 19:09:14 +0400

    mat mover

---
+++ Assets/Storm/Scripts/MatMover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MatMover : MonoBehaviour
+{
+    public Material mat;
+
+    public string offsetParamName;
+    
+    public Vector2 offsetVelocity;
+    
+    void Update()
+    {
+        mat.SetTextureOffset(offsetParamName, mat.GetTextureOffset(offsetParamName) + offsetVelocity * Time.deltaTime);
+    }
+}
